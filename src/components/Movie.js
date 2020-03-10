6ccb8a3962d9ebf47e859501ebd35805ac59ab58
Nedim Džibrić <47319999+Nedimm23@users.noreply.github.com>
2020-03-10 16:47:47 +0100

    movie info added with some content

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,17 +5,29 @@ import MovieInfoBar from "./elements/MovieInfoBar";
 import Actor from "./elements/Actor";
 import Grid from "./elements/Grid";
 import Spinner from "./elements/Spinner";
+import { useMovieFetch } from "./hooks/useMovieFetch";
 
 const Movie = ({ movieId }) => {
+  const [movie, loading, error] = useMovieFetch(movieId);
+  console.log(movie);
+
+  if (error) return <div>Something went wrong ...</div>;
+  if (loading) return <Spinner />;
+
   return (
     <React.Fragment>
-      <Navigation />
-      <MovieInfo />
-      <MovieInfo />
-      <Grid>
-        <Actor />
+      <Navigation movie={movie.original_title} />
+      <MovieInfo movie={movie} />
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header="Actors">
+        {movie.actors.map(actor => (
+          <Actor key={actor.credit_id} actor={actor} />
+        ))}
       </Grid>
-      <Spinner />
     </React.Fragment>
   );
 };
